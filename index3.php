46efbe146b046643df9e6ecab7caa1ead4f8d29f
cixwarebibek <60642185+cixwarebibek@users.noreply.github.com>
2020-02-25 14:35:24 +0545

    Update index3.php

--- index3.php
+++ index3.php
@@ -1,37 +1,6 @@
 <?php
 
-$fnum =5;
-$snum =10;
-$sum=$fnum + $snum;
-echo "Sum: ",$sum;
-echo "\n";
-$mul=$fnum*$snum;
-echo "mul: ",$mul; 
-echo "\n";
-$Div=$fnum/$snum;
-echo "Div: ",$Div; 
-echo "\n";
-$sub=$fnum-$snum;
-echo "sub: ",$sub; 
-echo "\n";
-const PI=3.14;
-$r= 5;
-$circum =2*PI*$r;
-echo "circumstances: ",$circum; 
-echo "\n";
-$area =PI*$r*$r;
-echo "area: ",$area;
-echo "\n";
-$p=1500;
-$t=2;
-$interest=$p*$t*$r/100;
-echo "simple interest: ",$interest;
-echo "\n";
-$ferhenheit=56;
-$process=($ferhenheit-32);
-$celcius=5/9*$process;
-echo "celcius: ",$celcius;
-echo "\n";
+
 
 $math= 50;
 $english=60;
